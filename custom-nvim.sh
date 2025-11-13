d2ad67fcb6b54a239546ab32bf541317a7046644
yeshey tenzin <yesheytenzin09@gmail.com>
2025-11-13 22:22:01 +0600

    nvim script

---
+++ custom-nvim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -r ~/personal/dev-tool/nvim ~/.config
+echo "success"
--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -3,7 +3,7 @@ return {
     "numToStr/Comment.nvim",
     config = function()
       require('Comment').setup()
-      vim.keymap.set('n', '<Leader>/', ':CommentToggle<CR>')
+      vim.keymap.set('n', '<Leader>c', ':CommentToggle<CR>')
     end
   }
 }
