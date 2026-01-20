8d8b6d60c999e8df8521e565daed680fcf642d3d
yeshey tenzin <yesheytenzin09@gmail.com>
2026-01-20 11:08:19 +0600

    neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,14 +3,14 @@ require("config.lazy")
 require("config.colors")
 require("config.options")
 require("config.keymaps")
-
+require("config.diagnostics")
 -- Detect if inside tmux
-local inside_tmux = os.getenv("TMUX") ~= nil
+-- local inside_tmux = os.getenv("TMUX") ~= nil
 
-if inside_tmux then
-  vim.opt.laststatus = 0   -- hide statusline in tmux
-else
-  vim.opt.laststatus = 3   -- global statusline outside tmux
-  require("config.lualine")
-end
+-- if inside_tmux then
+  -- vim.opt.laststatus = 0   -- hide statusline in tmux
+-- else
+  -- vim.opt.laststatus = 3   -- global statusline outside tmux
+  -- require("config.lualine")
+-- end
 
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,13 +1,29 @@
+-- return {
+  -- {
+    -- "nvim-lualine/lualine.nvim",
+    -- enabled = false, -- This disables the plugin completely
+    -- dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- config = function()
+      -- require('lualine').setup {
+        -- options = { theme = 'tokyonight' }
+      -- }
+    -- end
+  -- }
+-- }
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    enabled = false, -- This disables the plugin completely
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require('lualine').setup {
-        options = { theme = 'tokyonight' }
-      }
-    end
-  }
+	{
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        section_separators = "",
+        component_separators = "",
+        globalstatus = true, -- single bar across splits (recommended)
+      },
+    })
+  end
+	}
 }
 
